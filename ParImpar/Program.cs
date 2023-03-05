8973d6b22e9c7cd920dacdb6925b0039f564a497
Ari Mauri <ari.mauri.jr@gmail.com>
2023-03-05 11:27:43 -0300

    Add project files.

---
+++ ParImpar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double numero = 0.0;
+
+        Console.WriteLine("Digite um número");
+        numero = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        if (numero % 2 == 0.0)
+        {
+            Console.WriteLine("O número digitado é PAR");
+        }
+        else
+        {
+            Console.WriteLine("O número digitado é ÍMPAR");
+        }
+    }
+}
